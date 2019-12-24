42a24295845fa01f4cac90ad3909550d855e6c21
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2019-12-24 16:06:17 +0300

    Draft ClusteredNPeers topology

--- src/test/kotlin/io/libp2p/simulate/topology/RandomNPeers.kt
+++ src/test/kotlin/io/libp2p/simulate/topology/RandomNPeers.kt
@@ -1,25 +1,17 @@
 package io.libp2p.simulate.topology
 
-import io.libp2p.simulate.ImmutableNetworkImpl
-import io.libp2p.simulate.Network
-import io.libp2p.simulate.SimPeer
-import io.libp2p.simulate.Topology
+import org.jgrapht.Graph
 import org.jgrapht.generate.RandomRegularGraphGenerator
 import org.jgrapht.graph.DefaultUndirectedGraph
 import java.util.Random
 
-data class RandomNPeers(val peersCount: Int = 10) : Topology {
+data class RandomNPeers(val peersCount: Int = 10) : AbstractGraphTopology() {
     override var random = Random()
 
-    override fun connect(peers: List<SimPeer>): Network {
+    override fun <T> buildGraph(peers: List<T>): Graph<T, Any> {
         val peersVertex = peers.iterator()
-        val graph = DefaultUndirectedGraph(peersVertex::next, { Any() }, false)
-        RandomRegularGraphGenerator<SimPeer, Any>(peers.size, peersCount, random).generateGraph(graph)
-        val conns = peers
-            .flatMap { graph.incomingEdgesOf(it) }
-            .distinct()
-            .map { graph.getEdgeSource(it).connect(graph.getEdgeTarget(it)) }
-            .map { it.get() }
-        return ImmutableNetworkImpl(conns)
+        val graph: Graph<T, Any> = DefaultUndirectedGraph(peersVertex::next, { Any() }, false)
+        RandomRegularGraphGenerator<T, Any>(peers.size, peersCount, random).generateGraph(graph)
+        return graph
     }
 }
